ed78965e2393bf20409d79f5348c9d23a91159e3
loomis-hillel <89052159+loomis-hillel@users.noreply.github.com>
2023-08-16 18:45:05 -0400

    Update __init__.py

--- apcsa/u1/changemaker/__init__.py
+++ apcsa/u1/changemaker/__init__.py
@@ -1,6 +1,16 @@
 import check50
 
 @check50.check()
-def hello_world():
-  """hello world"""
-  check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
+def exists():
+  """File (ChangeMaker.java) Exists"""
+  check50.exists("ChangeMaker.java")
+
+@check50.check()
+def compiles(exists):
+  """File (ChangeMaker.java) Compiles"""
+  check50.run("javac ChangeMaker.java").exits("ChangeMaker.class")
+
+@check50.check()
+def test0(compiles):
+  """Test0"""
+  check50.run("java ChangeMaker").stdin("0.00\n0.00").stdout("Hello, world!", regex=False).exit(0)
